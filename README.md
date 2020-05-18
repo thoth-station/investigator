3c12dd4872e5835e56563e9f6df796bc5767205e
Francesco Murdaca <fmurdaca@redhat.com>
2020-05-18 19:15:37 +0200

    Update README
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repo contains two jobs:
 
-    PRODUCER: Query the database for unresolved packages that need to be solved with priorities for a user. (Run in Argo workflow task through Python S2I)
+    PRODUCER: Check if there are any unresolved packages in adviser report.
     It sends a message to Kafka with information relative to package and runtime environment to use for the solver to be scheduled.
 
     CONSUMER: Take messages from Kafka relative to package that need to be solved with priority and schedule solver for it.
--- unresolved_package_handler.py
+++ unresolved_package_handler.py
@@ -17,30 +17,20 @@
 
 """This is run to send messages regarding unresolved packages that need to be solved with priority."""
 
-import asyncio
 import logging
-import faust
-import os
-import ssl
 import json
-import sys
+import os
 
 from typing import Optional
 from pathlib import Path
 
-from thoth.messaging import UnresolvedPackageMessage
 from thoth.storages.graph import GraphDatabase
-
-_DRY_RUN = bool(int(os.getenv("DRY_RUN", 0)))
-
-if not _DRY_RUN:
-    app = MessageBase.app
 
 _LOGGER = logging.getLogger("thoth.unresolved_package_handler")
 
 
-def adviser_reporter(file_test_path: Optional[Path] = None):
-    """Run advise-reporter"""
+def unresolved_package_handler(file_test_path: Optional[Path] = None):
+    """Run Unresolved Package Handler"""
     if file_test_path:
         _LOGGER.debug("Dry run..")
         adviser_run_path = file_test_path
@@ -78,46 +68,17 @@ def adviser_reporter(file_test_path: Optional[Path] = None):
     operating_system = runtime_environment.get("operating_system", {})
     if operating_system:
         os_name = runtime_environment["operating_system"].get("name")
-        os_version=GraphDatabase()._normalize_os_version(operating_system.get("name"), operating_system.get("version"))
+        os_version=GraphDatabase().normalize_os_version(operating_system.get("name"), operating_system.get("version"))
         python_version = runtime_environment.get("python_version")
         solver = "solver" + "-" + os_name + "-" + os_version + "-" + "py" + "".join(runtime_environment["python_version"].split("."))
 
+        GraphDatabase().parse_python_solver_name(solver)
+
     package_version  = None
 
+    _LOGGER.info(f"Unresolved packages identified.. {unresolved_packages}")
+    _LOGGER.info(f"package versions identified.. {package_version}")
+    _LOGGER.info(f"Sources identified.. {sources}")
+    _LOGGER.info(f"Solver identified.. {solver}")
+
     return unresolved_packages, package_version, sources, solver
-
-@app.command()
-async def main():
-    """Run advise-reporter."""
-    unresolved_package = UnresolvedPackageMessage()
-    unresolved_packages, package_version, sources, solver = adviser_reporter()
-
-    for package_name in unresolved_packages:
-        try:
-            if not _DRY_RUN:
-                await unresolved_package.publish_to_topic(
-                    unresolved_package.MessageContents(
-                        package_name=package_name,
-                        package_version=package_version,
-                        index_url=sources,
-                        solver=solver,
-                    )
-                )
-            _LOGGER.info(
-                "Unresolved package:\npackage name:%r\npackage version:%r\nindex_url:%r\nruntime_environment:%r\n",
-                package_name,
-                package_version,
-                sources,
-                runtime_environment,
-            )
-
-        except Exception as identifier:
-            _LOGGER.exception("Failed to publish with the following error message: %r", identifier)
-
-
-if __name__ == "__main__":
-    if not _DRY_RUN:
-        app.main()
-    else:
-        file_test_path = Path.cwd().joinpath("", "adviser-04ab56d6.json")
-        adviser_reporter(file_test_path=file_test_path)
