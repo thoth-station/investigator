453458993c92ca78e2ed02f92eab1b31c589e670
Francesco Murdaca <fmurdaca@redhat.com>
2020-05-18 19:13:24 +0200

    Split main producer method for testing
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- unresolved_package_handler.py
+++ unresolved_package_handler.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# thoth-storages
+# thoth-unresolved-package-handler
 # Copyright(C) 2020 Francesco Murdaca
 #
 # This program is free software: you can redistribute it and / or modify
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""This is run to send messages regarding unsolved packages that need to be solved with priorities."""
+"""This is run to send messages regarding unresolved packages that need to be solved with priority."""
 
 import asyncio
 import logging
@@ -25,18 +25,32 @@ import ssl
 import json
 import sys
 
-from thoth.messaging import UnresolvedPackageMessage
+from typing import Optional
+from pathlib import Path
 
-app = MessageBase.app
+from thoth.messaging import UnresolvedPackageMessage
+from thoth.storages.graph import GraphDatabase
+
+_DRY_RUN = bool(int(os.getenv("DRY_RUN", 0)))
+
+if not _DRY_RUN:
+    app = MessageBase.app
 
 _LOGGER = logging.getLogger("thoth.unresolved_package_handler")
 
 
-@app.command()
-async def main():
-    """Run advise-reporter."""
-    adviser_run_path = os.environ["FILE_PATH"]
+def adviser_reporter(file_test_path: Optional[Path] = None):
+    """Run advise-reporter"""
+    if file_test_path:
+        _LOGGER.debug("Dry run..")
+        adviser_run_path = file_test_path
+    else:
+        adviser_run_path = os.environ["FILE_PATH"]
+
     sources_json_path = "result.parameters.project.requirements.source"
+
+    if not Path(adviser_run_path).exists():
+        raise FileNotFoundError(f"Cannot find the file on this path: {adviser_run_path}")
 
     with open(adviser_run_path, "r") as f:
         content = json.load(f)
@@ -49,7 +63,7 @@ async def main():
             unresolved_packages = errors_details["unresolved"]
 
     if not unresolved_packages:
-        _LOGGER.warning("No packages to be solved with priority")
+        _LOGGER.warning("No packages to be solved with priority identified.")
         sys.exit(2)
 
     sources = []
@@ -64,33 +78,46 @@ async def main():
     operating_system = runtime_environment.get("operating_system", {})
     if operating_system:
         os_name = runtime_environment["operating_system"].get("name")
-        os_version=_normalize_os_version(operating_system.get("name"), operating_system.get("version"))
+        os_version=GraphDatabase()._normalize_os_version(operating_system.get("name"), operating_system.get("version"))
         python_version = runtime_environment.get("python_version")
         solver = "solver" + "-" + os_name + "-" + os_version + "-" + "py" + "".join(runtime_environment["python_version"].split("."))
 
-    for unresolved_package in unresolved_packages:
-        if sources:
-            for index_url in sources:
-                try:
-                    await unresolved_package.publish_to_topic(
-                        unresolved_package.MessageContents(
-                            package_name=package_name,
-                            package_version=package_version,
-                            index_url=index_url,
-                            solver=solver,
-                        )
-                    )
-                    _LOGGER.debug(
-                        "Unresolved package:\npackage name:%r\npackage version:%r\nindex_url:%r\nruntime_environment:%r\n",
-                        package_name,
-                        package_version,
-                        index_url,
-                        runtime_environment,
-                    )
+    package_version  = None
 
-                except Exception as identifier:
-                    _LOGGER.exception("Failed to publish with the following error message: %r", identifier)
+    return unresolved_packages, package_version, sources, solver
+
+@app.command()
+async def main():
+    """Run advise-reporter."""
+    unresolved_package = UnresolvedPackageMessage()
+    unresolved_packages, package_version, sources, solver = adviser_reporter()
+
+    for package_name in unresolved_packages:
+        try:
+            if not _DRY_RUN:
+                await unresolved_package.publish_to_topic(
+                    unresolved_package.MessageContents(
+                        package_name=package_name,
+                        package_version=package_version,
+                        index_url=sources,
+                        solver=solver,
+                    )
+                )
+            _LOGGER.info(
+                "Unresolved package:\npackage name:%r\npackage version:%r\nindex_url:%r\nruntime_environment:%r\n",
+                package_name,
+                package_version,
+                sources,
+                runtime_environment,
+            )
+
+        except Exception as identifier:
+            _LOGGER.exception("Failed to publish with the following error message: %r", identifier)
 
 
 if __name__ == "__main__":
-    app.main()
+    if not _DRY_RUN:
+        app.main()
+    else:
+        file_test_path = Path.cwd().joinpath("", "adviser-04ab56d6.json")
+        adviser_reporter(file_test_path=file_test_path)
