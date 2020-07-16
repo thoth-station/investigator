79953d004bea5ae0b9ef8d2708507935cc924e44
Christoph Görn <goern@redhat.com>
2020-07-16 13:25:12 +0200

    a little more roubust app.sh (#76)
    * :sparkles: a little more roubust app.sh
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * cleaned up app.sh
    
    Signed-off-by: Christoph Görn <goern@redhat.com>
    
    * put the argument to the right command
    
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- app.sh
+++ app.sh
@@ -4,20 +4,22 @@
 # command and debug level
 #
 
-if [ "$SUBCOMMAND" = "producer" ]
-then
-    if [ "$DEBUG_LEVEL" -eq 1]
-    then
-        exec faust --debug --loglevel debug -A producer main
-    else
-        exec faust -A producer main
-    fi
-elif [ "$SUBCOMMAND" = "consumer" ]
-then
-    if [ "$DEBUG_LEVEL" -eq 1]
-    then
-        exec faust --debug --loglevel debug -A consumer worker
-    else
-        exec faust -A consumer worker
-    fi
+set -o nounset
+set -o errexit
+set -o errtrace
+set -o pipefail
+trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+
+SUBCOMMAND=${SUBCOMMAND:-producer}
+DEBUG_LEVEL=${DEBUG_LEVEL:-0}
+DEBUG_FLAGS=""
+
+if [ "$DEBUG_LEVEL" -eq 1 ]; then
+    DEBUG_FLAGS="--debug --loglevel debug"
+fi
+
+if [ "$SUBCOMMAND" == "producer" ]; then
+    exec faust ${DEBUG_FLAGS} -A producer main
+elif [ "$SUBCOMMAND" = "consumer" ]; then
+    exec faust ${DEBUG_FLAGS} -A consumer worker --web-host 0.0.0.0
 fi
