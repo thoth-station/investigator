80830cbb0ab955dda405216bddef33f997681bbe
Francesco Murdaca <fmurdaca@redhat.com>
2020-07-20 12:34:20 +0200

    Schedule different workflows
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/investigator/investigate_unresolved_package.py
+++ thoth/investigator/investigate_unresolved_package.py
@@ -34,6 +34,7 @@ from thoth.python import Pipfile
 from prometheus_client import CollectorRegistry, Gauge, push_to_gateway, Counter
 
 _LOG_SOLVER = os.environ.get("THOTH_LOG_SOLVER") == "DEBUG"
+_LOG_REVSOLVER = os.environ.get("THOTH_LOG_REVSOLVER") == "DEBUG"
 
 IN_PROGRESS_GAUGE = Gauge(
     "investigators_in_progress", "Total number of investigation messages currently being processed."
@@ -120,35 +121,74 @@ def parse_unresolved_package_message(unresolved_package: MessageBase) -> None:
     requested_indexes: Optional[List[str]] = unresolved_package.index_url
     solver = unresolved_package.solver
 
+    openshift = OpenShift()
+
     graph = GraphDatabase()
     graph.connect()
 
+    # Select indexes
     registered_indexes: List[str] = graph.get_python_package_index_urls_all()
 
     indexes = registered_indexes
 
     if not requested_indexes:
-        _LOGGER.info("Scheduling Thoth registered indexes...")
+        _LOGGER.info("using Thoth registered indexes...")
     else:
         if all(index_url in registered_indexes for index_url in requested_indexes):
             indexes = requested_indexes
             _LOGGER.info("Using requested indexes...")
         else:
-            _LOGGER.info("Scheduling Thoth registered indexes...")
+            _LOGGER.info("Using Thoth registered indexes...")
 
-    is_scheduled = _schedule_solver(
-        package_name=package_name, package_version=package_version, indexes=indexes, solver=solver
-    )
+    # Select solvers
+    if not solver:
+        solvers: List[str] = openshift.get_solver_names()
+    else:
+        solvers = [solver]
 
-    # TODO: Expose metrics instead of sending to Pushgateway
-    send_metrics_to_pushgateway(unresolved_package=unresolved_package, is_scheduled=is_scheduled)
+    # Check package version for each index
+
+    for index_url in indexes:
+
+        is_present = graph.python_package_version_exists(
+            package_name=package_name, package_version=package_version, index_url=index_url, solver_name=solver_name
+        )
+
+        if is_present:
+
+            # Check for which solver has been already solved
+            for solver_name in solvers:
+                is_solved = graph.python_package_version_exists(
+                    package_name=package_name,
+                    package_version=package_version,
+                    index_url=index_url,
+                    solver_name=solver_name,
+                )
+                if not is_solved:
+                    is_scheduled = _schedule_solver(
+                        openshift=openshift,
+                        package_name=package_name,
+                        package_version=package_version,
+                        indexes=[index_url],
+                        solver_name=solver_name,
+                    )
+
+        else:
+            are_scheduled = _schedule_all_solvers(
+                package_name=package_name, package_version=package_version, indexes=[index_url]
+            )
+
+            is_revsolver_scheduled = _schedule_revsolver(package_name=package_name, package_version=package_version)
+
+            is_si_analyzer_scheduled = _schedule_security_indicator(package_name=package_name, package_version=package_version, index_url=index_url)
+
     SUCCESSES_COUNTER.inc()
 
 
-def _schedule_solver(package_name: str, package_version: str, indexes: List[str], solver: str) -> int:
+def _schedule_solver(
+    openshift: Openshift, package_name: str, package_version: str, indexes: List[str], solver_name: str
+) -> int:
     """Schedule solver."""
-    openshift = OpenShift()
-
     try:
         if not package_version:
             packages = package_name
@@ -156,31 +196,81 @@ def _schedule_solver(package_name: str, package_version: str, indexes: List[str]
             packages = f"{package_name}==={package_version}"
 
         analysis_id = openshift.schedule_solver(
-            solver=solver, packages=packages, indexes=indexes, transitive=False, debug=_LOG_SOLVER
+            solver=solver_name, packages=packages, indexes=indexes, transitive=False, debug=_LOG_SOLVER
         )
         _LOGGER.info(
             "Scheduled solver %r for packages %r from indexes %r, analysis is %r",
-            solver,
+            solver_name,
             packages,
             indexes,
             analysis_id,
         )
         is_scheduled = 1
     except Exception as e:
-        _LOGGER.warning(f"Failed to schedule solver for package {packages} from {indexes}: {e}")
+        _LOGGER.warning(f"Failed to schedule solver {solver_name} for package {packages} from {indexes}: {e}")
         is_scheduled = 0
 
     return is_scheduled
 
 
-def send_metrics_to_pushgateway(unresolved_package: MessageBase, is_scheduled: int) -> None:
-    """Send metrics to Pushgateway."""
-    _METRIC_UNRESOLVED_TYPE.labels(package_name=unresolved_package.package_name).set(is_scheduled)
-    _LOGGER.info("unresolved_package(%r)=%r", unresolved_package.package_name, is_scheduled)
+def _schedule_all_solvers(openshift: OpenShift, package_name: str, package_version: str, indexes: List[str]) -> int:
+    """Schedule all solvers."""
+    try:
+        analysis_ids = openshift.schedule_all_solvers(packages=f"{package_name}==={package_version}", indexes=indexes)
+        _LOGGER.info(
+            "Scheduled solver %r for packages %r from indexes %r, analysis is %r", packages, indexes, analysis_ids
+        )
+        are_scheduled = len(analysis_ids)
+    except Exception:
+        _LOGGER.warning(f"Failed to schedule solvers for package {packages} from {indexes}")
+        are_scheduled = 0
 
-    if _THOTH_METRICS_PUSHGATEWAY_URL:
+    return are_scheduled
+
+
+def _schedule_revsolver(openshift: OpenShift, package_name: str, package_version: str) -> int:
+    """Schedule revsolver."""
+    if package_version and (package_name, package_version) not in revsolver_packages_seen:
         try:
-            _LOGGER.info(f"Submitting metrics to Prometheus pushgateway {_THOTH_METRICS_PUSHGATEWAY_URL}")
-            push_to_gateway(_THOTH_METRICS_PUSHGATEWAY_URL, job="unresolved-package", registry=prometheus_registry)
-        except Exception as e:
-            _LOGGER.info(f"An error occurred pushing the metrics: {str(e)}")
+            analysis_id = openshift.schedule_revsolver(
+                package_name=package_name, package_version=package_version, debug=_LOG_REVSOLVER
+            )
+            _LOGGER.info(
+                "Scheduled reverse solver for package %r in version %r, analysis is %r",
+                package_name,
+                package_version,
+                analysis_id,
+            )
+            is_scheduled = 1
+        except Exception:
+            _LOGGER.warning("Failed to schedule reverse solver for %r in version %r", package_name, package_version)
+            is_scheduled = 0
+            continue
+
+    return is_scheduled
+
+def _schedule_security_indicator(package_name: str, package_version: str, index_url: str) -> int:
+    """Schedule Security Indicator."""
+    openshift = OpenShift()
+    try:
+        analysis_id = openshift.schedule_security_indicator(
+            python_package_name=package_name,
+            python_package_version=package_version,
+            python_package_index=index_url,
+            aggregation_function="process_data",
+        )
+        _LOGGER.info(
+            "Scheduled SI %r for package %r in version %r from index %r, analysis is %r",
+            package_name,
+            package_version,
+            index_url,
+            analysis_id,
+        )
+        is_scheduled = 1
+    except Exception:
+        _LOGGER.warning(
+            f"Failed to schedule SI for package {package_name} in version {package_version} from index {index_url}"
+        )
+        is_scheduled = 0
+
+    return is_scheduled
