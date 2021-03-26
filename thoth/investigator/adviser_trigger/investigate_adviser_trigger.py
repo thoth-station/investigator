56c1e017db4388ce3d738c47b65fd9c7b84dcd8c
Kevin Postlethwait <k.postlethwait24@gmail.com>
2021-03-26 16:20:13 -0400

    use function as handler for all versions of message

--- thoth/investigator/adviser_trigger/investigate_adviser_trigger.py
+++ thoth/investigator/adviser_trigger/investigate_adviser_trigger.py
@@ -34,7 +34,7 @@ from prometheus_async.aio import track_inprogress, count_exceptions
 _LOGGER = logging.getLogger(__name__)
 
 
-@register_handler(AdviserTriggerMessage().topic_name, ["v2", "v3"])
+@register_handler(AdviserTriggerMessage().topic_name, ["v1", "v2", "v3"])
 @count_exceptions(adviser_trigger_exceptions)
 @track_inprogress(adviser_trigger_in_progress)
 async def parse_adviser_trigger_message(adviser_trigger: Dict[str, Any], openshift: OpenShift, **kwargs) -> None:
